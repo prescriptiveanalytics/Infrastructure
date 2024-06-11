190e7f16eeec630152a9baf27d6a9c67e4afccf2
Florian Bachinger <flo.bachinger@gmail.com>
2024-06-11 12:16:52 +0200

    file must be initially created by runner to remember state

--- kubernetes/terraform/vm_cloud_image.tf
+++ kubernetes/terraform/vm_cloud_image.tf
@@ -3,5 +3,5 @@ resource "proxmox_virtual_environment_download_file" "CentOS_cloud_image" {
   datastore_id = "local"
   node_name    = var.node_name
   url          = "https://cloud.centos.org/centos/8-stream/x86_64/images/CentOS-Stream-GenericCloud-8-latest.x86_64.qcow2"
-  file_name    = "CentOS8-genericcloud.img"
+  file_name    = "CentOS8-genericcloud_managed_by_spa_terraform_runner_do_not_delete.img"
 }
